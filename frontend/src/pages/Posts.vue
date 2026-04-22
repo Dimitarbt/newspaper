7d54d25202a301176074027b3a0c234e5e22df8e
Dimitar Fidanovski <fdimitar@hotmail.com>
2026-04-22 14:54:48 +0200

    define style and get all post from backend

--- frontend/src/pages/Posts.vue
+++ frontend/src/pages/Posts.vue
@@ -1,8 +1,37 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import axiosClient from '../axios';
+
+
+const posts = ref([])
+const loading = ref(false);
+
+onMounted(() => {
+    axiosClient.get('/posts')
+    .then(response => {
+        console.log(response.data)
+    })
+})
+
+function getAllPosts(){
+    alert('getAllPosts');
+}
+
 
 </script>
 
 <template>
-    <p class="text-red-100">Posts</p>
-    <h1 class="text-red-500 text-3xl">TEST</h1>
+    <h1 class="text-red-500 text-3xl">Posts</h1>
+    <div>
+     <article class="max-w-2xl mx-auto p-4">
+        <header class="mb-8">
+            <h1 class="text-4xl font-bold text-gray-900 mb-2">How to Use Tailwind CSS</h1>
+            <p class="text-gray-600">Published on May 20, 2026</p>
+        </header>
+
+        <div class="prose prose-blue max-w-none">
+            <p>Tailwind CSS is a utility-first CSS framework...</p>
+        </div>
+     </article>
+    </div>
 </template>
