4423eb27136be8819b9689eae3ed8e7b091b43e4
Dimitar Fidanovski <fdimitar@hotmail.com>
2026-04-22 14:33:36 +0200

    defining tailwind css

--- frontend/src/pages/Posts.vue
+++ frontend/src/pages/Posts.vue
@@ -3,4 +3,6 @@
 </script>
 
 <template>
+    <p class="text-red-100">Posts</p>
+    <h1 class="text-red-500 text-3xl">TEST</h1>
 </template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 import Posts from './pages/Posts.vue';
 const routes = [
   {
-    path:'posts', name:"posts", component:Posts
+    path:'/posts', name:"posts", component:Posts
   }
 ];
 
