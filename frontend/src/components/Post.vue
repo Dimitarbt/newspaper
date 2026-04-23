4c383b2b13e590784a25b5b8bf904e71c1f96551
Dimitar Fidanovski <fdimitar@hotmail.com>
2026-04-23 14:23:53 +0200

    some frontend changes

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -7,12 +7,14 @@ const props = defineProps({
 
 <template>
     <div>
-     <article class="max-w-2xl mx-auto p-4" v-for="post in posts" :key="post.id">
+     <article class="max-w-2xl mx-auto p-4 mt-10 bg-white rounded-2xl shadow-xl" v-for="post in posts" :key="post.id">
         <header class="mb-8">
             <h1 class="text-4xl font-bold text-gray-900 mb-2">{{ post.title }}</h1>
             <p class="text-gray-600">Published on {{ post.created_at }}</p>
         </header>
-
+        <div v-if="post.post_image" class="mb-6">
+            <img :src="post.post_image" class="rounded-lg w-48 h-36"  />
+        </div>
         <div class="prose prose-blue max-w-none">
             <p>{{ post.body }}</p>
         </div>
