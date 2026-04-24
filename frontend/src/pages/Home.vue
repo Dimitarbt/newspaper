b952a3de387fc67a70d9547f97ed9023298275a2
Dimitar Fidanovski <fdimitar@hotmail.com>
2026-04-24 14:22:28 +0200

    define frontend routes and component for register and login

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+
+<p>Home</p>
+<RouterLink :to="{name:'login'}">Login</RouterLink>
+<RouterLink :to="{name:'register'}">Register</RouterLink>
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,27 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Posts from './pages/Posts.vue';
 import FormPost from './pages/FormPost.vue';
+import Login from './pages/Login.vue';
+import Home from './pages/Home.vue';
+import Register from './pages/Register.vue';
 const routes = [
+  {
+    path: '/', name: 'home', component: Home
+  },
   {
     path:'/posts', name:"posts", component:Posts,
   },
   {    
     path:'/create-post', name: "create-post", component:FormPost
   },
-   {    
+  {    
     path:'/edit-post/:id', name: "edit-post", component:FormPost, params:true
+  },
+  {
+    path: '/login', name: 'login', component: Login
+  },
+  {
+    path: '/register', name: 'register', component: Register
   }
 ];
 
